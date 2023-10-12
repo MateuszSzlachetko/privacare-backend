510c1087fa390c856b2fd844cf12b594fbde4142
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2023-10-12 12:18:27 +0200

    chore: project initialization

---
+++ src/main/java/com/privacare/PrivacareApplication.java
@@ -0,0 +1,13 @@
+package com.privacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/privacare/PrivacareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
